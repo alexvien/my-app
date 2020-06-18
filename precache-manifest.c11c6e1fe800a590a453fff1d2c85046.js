0c826b6b67ce839882b419cb740dbe76022188ef
alexvien <66107566+alexvien@users.noreply.github.com>
2020-06-18 02:31:36 -0700

    Updates

--- precache-manifest.c11c6e1fe800a590a453fff1d2c85046.js
+++ precache-manifest.c11c6e1fe800a590a453fff1d2c85046.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "18e5164959d654afae69e7161ea3b810",
+    "revision": "563b1eb15aaa11184944082c32915577",
     "url": "/my-app/index.html"
   },
   {
-    "revision": "f211b7add5a91abd322d",
+    "revision": "ad733d94c5c3d46f4f51",
     "url": "/my-app/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "48c1aaded07c14fd5dad",
+    "revision": "35d019ab972b78d96670",
     "url": "/my-app/static/css/main.b0572622.chunk.css"
   },
   {
-    "revision": "f211b7add5a91abd322d",
-    "url": "/my-app/static/js/2.3213f804.chunk.js"
+    "revision": "ad733d94c5c3d46f4f51",
+    "url": "/my-app/static/js/2.ea7388dd.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/my-app/static/js/2.3213f804.chunk.js.LICENSE.txt"
+    "url": "/my-app/static/js/2.ea7388dd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "48c1aaded07c14fd5dad",
-    "url": "/my-app/static/js/main.b7e180c7.chunk.js"
+    "revision": "35d019ab972b78d96670",
+    "url": "/my-app/static/js/main.4ca9a0bc.chunk.js"
   },
   {
     "revision": "660024b2ea62aa0a0b7b",
