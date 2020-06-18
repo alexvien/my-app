85e2e7debd4a81f06057cb1bf11afe4cce5d95f1
alexvien <66107566+alexvien@users.noreply.github.com>
2020-06-18 02:15:40 -0700

    Updates

---
+++ precache-manifest.753bd3cdab2b0a701e0a159618a76204.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e5164959d654afae69e7161ea3b810",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f211b7add5a91abd322d",
+    "url": "/my-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "48c1aaded07c14fd5dad",
+    "url": "/my-app/static/css/main.b0572622.chunk.css"
+  },
+  {
+    "revision": "f211b7add5a91abd322d",
+    "url": "/my-app/static/js/2.3213f804.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-app/static/js/2.3213f804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48c1aaded07c14fd5dad",
+    "url": "/my-app/static/js/main.b7e180c7.chunk.js"
+  },
+  {
+    "revision": "660024b2ea62aa0a0b7b",
+    "url": "/my-app/static/js/runtime-main.5119dbb0.js"
+  }
+]);
